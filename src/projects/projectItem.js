e0682d4ffd59f5dcebf9afc9d59d2cb2e55a2f4b
Sriram <sgcuber24@gmail.com>
2020-05-18 17:53:54 +0530

    Added project title outside the image

--- src/projects/projectItem.js
+++ src/projects/projectItem.js
@@ -7,32 +7,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function ProjectItem(props) {
   let project = props.project;
   return (
-    <div className="rounded-lg pointer-events-auto relative w-full h-full">
-      <img
-        src={project['image']}
-        alt="Mockup"
-        className="object-fill rounded-lg h-full w-full"
-      />
-      <div
-        className="transition duration-300 ease-in-out absolute opacity-0 hover:opacity-100 top-0 right-0 bottom-0 left-0 rounded-lg flex flex-col align-middle content-center justify-center"
-        style={{ backgroundColor: '#2F2F2F' }}
-      >
-        <h1 className="text-white text-xl font-bold text-center">
-          {project['title']}
-        </h1>
-        <p className="text-white text-xs font-bold p-4 text-center">
-          {project['description']}
-        </p>
-        <div className="w-full text-center">
-          <a
-            href={project['github']}
-            target="_blank"
-            className="pointer-events-auto text-center w-32 h-8"
-          >
-            <button className="text-md font-bold text-white outline-none bg-custom-blue-button active:bg-blue-700 hover:bg-blue-500 rounded-md w-32 h-8 tracking-wide">
-              <FontAwesomeIcon icon={faGithub} /> GitHub
-            </button>
-          </a>
+    <div>
+      <h1 className="text-white text-2xl font-bold text-center">
+        {project['title']}
+      </h1>
+      <div className="rounded-lg pointer-events-auto relative w-full h-full mt-4">
+        <img
+          src={project['image']}
+          alt="Mockup"
+          className="object-fill rounded-lg h-full w-full"
+        />
+        <div
+          className="transition duration-300 ease-in-out absolute opacity-0 hover:opacity-100  right-0 bottom-0 h-full w-full rounded-lg flex flex-col align-middle content-center justify-center"
+          style={{ backgroundColor: '#2F2F2F' }}
+        >
+          <h1 className="text-white text-xl font-bold text-center">
+            {project['title']}
+          </h1>
+          <p className="text-white text-xs font-bold p-4 text-center">
+            {project['description']}
+          </p>
+          <div className="w-full text-center">
+            <a
+              href={project['github']}
+              target="_blank"
+              className="pointer-events-auto text-center w-32 h-8"
+            >
+              <button className="text-md font-bold text-white outline-none bg-custom-blue-button active:bg-blue-700 hover:bg-blue-500 rounded-md w-32 h-8 tracking-wide">
+                <FontAwesomeIcon icon={faGithub} /> GitHub
+              </button>
+            </a>
+          </div>
         </div>
       </div>
     </div>
