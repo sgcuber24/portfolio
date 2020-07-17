0166b59d7ee9b482975e3660392f6e6ff252a5bd
G Sriram <sgcuber24@gmail.com>
2020-07-17 12:34:58 +0530

    SeeFood change
    Co-authored-by: Sai Ashish <saiashish90@gmail.com>

--- src/projects/projectData.js
+++ src/projects/projectData.js
@@ -6,48 +6,42 @@ import seeFood from '../assets/mockups/SeeFood.png';
 import stdModel from '../assets/mockups/StdModelScreenshot.png';
 
 const projectData = [
-  {
-    title: 'SeeFood',
-    description:
-      'An App built using Flutter and Tensorflow Lite to detect 10 different food classes.',
-    image: seeFood,
-    github: 'https://github.com/sgcuber24/see_food',
-  },
-  {
-    title: 'EasyTravel',
-    description:
-      'An App built using React Native to demonstrate a digital ticketing system.',
-    image: easyTravel,
-    github: 'https://github.com/sgcuber24/EasyBMTC',
-  },
-  {
-    title: 'HobbyIt',
-    description:
-      'An App built using React Native and Firebase to connect people of similar interests.',
-    image: hobbyIt,
-    github: 'https://github.com/sgcuber24/hobbyit',
-  },
-  {
-    title: 'ICLog',
-    description:
-      'A Desktop Application built using PyQT5 and MySQL to manage labratory components.',
-    image: icLog,
-    github: 'https://github.com/sgcuber24/ic-log',
-  },
-  {
-    title: 'EzHealth',
-    description:
-      'A web app built using Flask to predict the requirement of mental health treatment.',
-    image: ezHealth,
-    github: 'https://github.com/sgcuber24/ez-health',
-  },
-  {
-    title: 'Standard Model',
-    description:
-      'An interactive webpage showing the standard model of particle physics.',
-    image: stdModel,
-    github: 'https://github.com/sgcuber24/standard-model',
-  },
+	{
+		title: 'SeeFooooooood',
+		description: 'An App built using Flutter and Tensorflow Lite to detect 10 different food classes.',
+		image: seeFood,
+		github: 'https://github.com/sgcuber24/see_food'
+	},
+	{
+		title: 'EasyTravel',
+		description: 'An App built using React Native to demonstrate a digital ticketing system.',
+		image: easyTravel,
+		github: 'https://github.com/sgcuber24/EasyBMTC'
+	},
+	{
+		title: 'HobbyIt',
+		description: 'An App built using React Native and Firebase to connect people of similar interests.',
+		image: hobbyIt,
+		github: 'https://github.com/sgcuber24/hobbyit'
+	},
+	{
+		title: 'ICLog',
+		description: 'A Desktop Application built using PyQT5 and MySQL to manage labratory components.',
+		image: icLog,
+		github: 'https://github.com/sgcuber24/ic-log'
+	},
+	{
+		title: 'EzHealth',
+		description: 'A web app built using Flask to predict the requirement of mental health treatment.',
+		image: ezHealth,
+		github: 'https://github.com/sgcuber24/ez-health'
+	},
+	{
+		title: 'Standard Model',
+		description: 'An interactive webpage showing the standard model of particle physics.',
+		image: stdModel,
+		github: 'https://github.com/sgcuber24/standard-model'
+	}
 ];
 
 export default projectData;
